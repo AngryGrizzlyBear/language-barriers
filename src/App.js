4c71a102237e2cfd3d17dd0b595b1ccf5fcd5197
Evan <evanmlongwood@gmail.com>
2019-10-31 14:51:42 -0700

    Added Word Table.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './index.css'
 import UserTable from './Components/UserTable'
 
+
 const App = () => {
+
+  const wordData= [
+    {id: 1, word:'はい', translation:'yes'},
+    {id: 2, word:'わたし', translation:'I'},
+    {id: 3, word:'あなた', translation:'You'},
+  ]
+
+  const [words, setWords] = useState(wordData)
+
+
+  
   return (
     <div className="container">
       <h1>Language Barriers</h1>
@@ -12,7 +24,7 @@ const App = () => {
         </div>
         <div className="flex-large">
           <h2>Translation</h2>
-          <UserTable />
+          <UserTable words={words} />
         </div>
       </div>
     </div>
