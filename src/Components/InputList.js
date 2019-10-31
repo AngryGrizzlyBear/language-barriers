4aed48dd53e36105cacd9706add43ed9a26a32e1
Evan <evanmlongwood@gmail.com>
2019-10-31 12:39:16 -0700

    Clean up

---
+++ src/Components/InputList.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const List = () => {
+return (
+    <table>
+        <thead>
+            <tr>Word</tr>
+            <tr>Translation</tr>
+        </thead>
+    </table>
+)
+}
+
+export default List
