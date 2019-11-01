f89047d8ae2810ed63ef4be1490fabf3a477da63
Evan <evanmlongwood@gmail.com>
2019-11-01 10:58:13 -0700

    add-new-word-and-definition

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
-import React, { useState } from 'react';
-import './index.css'
-import UserTable from './Components/UserTable'
-
+import React, { useState } from "react";
+import "./index.css";
+import UserTable from "./Components/UserTable";
+import AddWordForm from "./Components/AddWordForm"
 
 const App = () => {
+  const wordData = [
+    { id: 1, word: "はい", translation: "yes" },
+    { id: 2, word: "わたし", translation: "I" },
+    { id: 3, word: "あなた", translation: "You" }
+  ];
 
-  const wordData= [
-    {id: 1, word:'はい', translation:'yes'},
-    {id: 2, word:'わたし', translation:'I'},
-    {id: 3, word:'あなた', translation:'You'},
-  ]
+  const [words, setWords] = useState(wordData);
 
-  const [words, setWords] = useState(wordData)
+  const addWord = word => {
+    word.id = word.length + 1;
+    setWords([...words, word]);
+  };
 
-
-  
   return (
     <div className="container">
       <h1>Language Barriers</h1>
       <div className="flex-row">
         <div className="flex-large">
           <h2>Add Word</h2>
+          <AddWordForm addWord={addWord} />
         </div>
         <div className="flex-large">
           <h2>Translation</h2>
@@ -28,7 +31,7 @@ const App = () => {
         </div>
       </div>
     </div>
-    )
-}
+  );
+};
 
 export default App;
